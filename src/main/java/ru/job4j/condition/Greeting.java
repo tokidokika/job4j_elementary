8e2ff2837f912f678e8c78edb3dfc42a02a60045
Kate Sm <yukaicat@gmail.com>
2021-10-02 13:28:55 +0300

    9.1. String [#237407] исправление

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -2,8 +2,11 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += " But I am newbie. ";
         int year = 2021;
-        String idea = "I like Java! " + "But I am a newbie. " + year;
+        idea += year;
         System.out.println(idea);
     }
 }
