d1eb2eb404743babbc52258ae3a138a447e37f30
Kate Sm <yukaicat@gmail.com>
2021-10-14 16:55:41 +0300

    6.4.2. Слово заканчивается на ... [#156317] добавление break

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -6,6 +6,7 @@ public class EndsWith {
         for (int i = 0; i < post.length; i++) {
             if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
                 result = false;
+                break;
             }
         }
         return result;
