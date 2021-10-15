3e45a049ebec8335cdf50816b088cb80aced59fc
Kate Sm <yukaicat@gmail.com>
2021-10-15 16:06:51 +0300

    6.5. Поиск индекса в диапазоне. [#156318] исправление цикла

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -14,12 +14,10 @@ public class FindLoop {
 
     public static int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1;
-        for (int i = 0; i < data.length; i++) {
-            if (i >= start && i <= finish) {
-                if (data[i] == el) {
-                    rst = i;
-                    break;
-                }
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
             }
         }
         return rst;
